31350fd23825a3dd174d84e76b8ce0f5b2f85354
Jamin <tiedyeguy18@gmail.com>
2023-06-23 09:55:07 -0400

    Auto-ranging for sliding functions

--- README.md
+++ README.md
@@ -27,14 +27,12 @@ If you want to run this project locally, follow these instructions:
 
 The script can be run as follows:
 
-```$ main.py <F_LATEX> <F_EXPR> <G_LATEX> <G_EXPR> <FG_PROD_Y_MIN> <FG_PROD_Y_MAX> <T_MIN> <T_MAX> <T_STEP> <ANIMATION_QUALITY>```
+```$ main.py <F_LATEX> <F_EXPR> <G_LATEX> <G_EXPR> <T_MIN> <T_MAX> <T_STEP> <ANIMATION_QUALITY>```
 where
 
 - f and g are functions of t
 - `<X_LATEX>` is a LaTeX string representation of the function. For tau-subttution to work properly, you must surround your dependent variable t with spaces
 - `<X_EXPR>` is a Python expression representing the value of the function w.r.t. t
-- `<FG_PROD_Y_MIN/MAX>` are numbers constituting the range of the product of f and g in the specified domain (this needs
-  specification because it is very expensive to calculate at runtime)
 - `<T_MIN/MAX/STEP>` are numbers constituting the domain in which you'd like to convolve f and g, and step essentially
   gives the resolution of the function (how often a point is plotted)
 - `<ANIMATION_QUALITY>` is one of `low_quality`, `medium_quality`, or `high_quality`, which determines the pixel
@@ -45,21 +43,18 @@ You may need to wait a few minutes for the animation to render, mostly depending
 See the below for some examples.
 
 ## Demo
-`main.py "-\Lambda( t +1)+\Lambda( t -1)" "t if abs(t)<=1 else (-2-t if -2<t<-1 else (2-t if 2>t>1 else 0))" "\Pi(0.2 t )" "1 if abs(t) < 0.2 else 0" -1.25 1.25 -2 2 0.01 low_quality`
+`main.py "-\Lambda( t +1)+\Lambda( t -1)" "t if abs(t)<=1 else (-2-t if -2<t<-1 else (2-t if 2>t>1 else 0))" "\Pi(0.2 t )" "1 if abs(t) < 0.2 else 0" -2 2 0.01 low_quality`
 
 https://github.com/jamin-eisenberg/ConvolutionAnimation/assets/117237268/fb376412-d72e-4139-b109-a3523a109021
 
 
 
-`main.py "\cos(2 t )\Pi( t )" "math.cos(2*t) if abs(t) <= 0.5 else 0" "10u( t )" "10 if t >= 0 else 0" -11 11 -4 4 0.01 low_quality`
+`main.py "\cos(2 t )\Pi( t )" "math.cos(2*t) if abs(t) <= 0.5 else 0" "10u( t )" "10 if t >= 0 else 0" -4 4 0.01 low_quality`
 
 https://github.com/jamin-eisenberg/ConvolutionAnimation/assets/117237268/1c2d6d71-83ed-4376-a8ae-cdb800795e22
 
 
 
-`main.py "e^{-0.5 t }" "math.e**(-0.5 * t)" "2\delta( t -1)" "2 if abs(t - 1) < 0.01 else 0" 0 1.5 0 3 0.01 low_quality` 
+`main.py "e^{-0.5 t }" "math.e**(-0.5 * t)" "2\delta( t -1)" "2 if abs(t - 1) < 0.01 else 0" 0 3 0.005 low_quality`
 
 https://github.com/jamin-eisenberg/ConvolutionAnimation/assets/117237268/66dd4582-a46d-44b0-9f4c-3c7a5da5afb2
-
-(FIX)
-
--- main.py
+++ main.py
@@ -47,6 +47,15 @@ def get_func_range(func: Callable[[float], float], x_opts: (float, float, float)
     return [min(func_vals) - 0.005, max(func_vals) + 0.005]  # if the function is fully flat, add some dimension
 
 
+def get_nested_func_range(f, x_opts):
+    f_min, f_max = get_func_range(f(x_opts[0]), x_opts)
+    for t in frange(*x_opts):
+        l_min, l_max = get_func_range(f(t), x_opts)
+        f_min = min(f_min, l_min)
+        f_max = max(f_max, l_max)
+    return f_min, f_max
+
+
 # plots the given function f with the given color and the given x options and y-range, and returns
 # the axes the function was plotted on (transformed by axes_func) and the function itself
 def plot(f, f_color, x_opts, axes_func, y_range=None, **kwargs) -> (Axes, Mobject):
@@ -65,8 +74,7 @@ class AnimatedConvolution(Scene):
     # x_opts is an array of [x_min, x_max, x_step] used for plotting all functions
     # prod_y_range provides a range in which to display the product of the functions, as that is difficult to
     # pre-calculate
-    def __init__(self, eq1, eq2, eq1_str: str, eq2_str: str, x_opts: [float, float, float],
-                 prod_y_range: [float, float]):
+    def __init__(self, eq1, eq2, eq1_str: str, eq2_str: str, x_opts: [float, float, float]):
         super().__init__()
         self.eq1 = eval("lambda t: " + eq1)
         self.eq2 = eval("lambda t: " + eq2)
@@ -74,7 +82,6 @@ class AnimatedConvolution(Scene):
         self.eq2_str = eq2_str
         self.x_min, self.x_max, self.x_step = x_opts
         self.x_opts = x_opts
-        self.prod_y_range = prod_y_range
 
     # creates the components for and plays the scene
     def construct(self):
@@ -85,12 +92,10 @@ class AnimatedConvolution(Scene):
         titlef1 = make_plot_title(axf, f"f(t) = {self.eq1_str}")
         titlef2 = make_plot_title(axf, rf"f({TAU_LATEX}) = {self.eq1_str.replace(' t ', TAU_LATEX)}")
 
-        # sets up the value used to change the position of g (eq2) and the shape of f times g
-        t = ValueTracker(0)
         # sets up the second (reversed) value of the function g
         g2_func = lambda tau: self.eq2(t.get_value() - tau)
-        # get the range of g when t = 0
-        g2_range = get_func_range(g2_func, self.x_opts)
+        # get the range with a swept value of t
+        g2_range = get_nested_func_range(lambda t: lambda tau: self.eq2(t - tau), self.x_opts)
         # prepare t for sweeping from x_min to x_max
         t = ValueTracker(self.x_min)
 
@@ -109,11 +114,12 @@ class AnimatedConvolution(Scene):
 
         # get the product function (relying on the value t)
         prod_func = modulate(self.eq1, lambda tau: self.eq2(t.get_value() - tau))
+        prod_range = get_nested_func_range(lambda t: modulate(self.eq1, lambda tau: self.eq2(t - tau)), self.x_opts)
 
         def plot_prod():
             return plot(prod_func, GREEN,
                         self.x_opts, lambda x: x.next_to(axg, direction=DOWN, buff=3 * DEFAULT_BUFF),
-                        y_range=self.prod_y_range)
+                        y_range=prod_range)
 
         ax_prod, graph_prod = plot_prod()
 
@@ -126,7 +132,8 @@ class AnimatedConvolution(Scene):
         # plot the convolution of the functions on the right
         convolution_func = convolve(self.eq1, self.eq2, self.x_opts)
         ax_conv, graph_conv = plot(convolution_func, PURPLE, self.x_opts,
-                                   lambda x: x.move_to(RIGHT * config.frame_width / 4), y_length=4)
+                                   lambda x: x.move_to(RIGHT * config.frame_width / 4),
+                                   y_length=config.frame_height / 2)
 
         # just display the definition (symbolic calculation would be crazy)
         convolution_def_latex = rf"(f \ast g)(t)=\int_{{-\infty}}^{{\infty}} f({TAU_LATEX}) g(t-{TAU_LATEX}) d {TAU_LATEX}"
@@ -151,24 +158,21 @@ class AnimatedConvolution(Scene):
 
 
 def main():
-    if not 10 <= len(sys.argv) <= 11:
+    if not 8 <= len(sys.argv) <= 9:
         print('Invalid command line arguments. Please consult the README.')
         return
 
     with tempconfig(
-            {"quality": list_get(sys.argv, 10, "medium_quality"), "preview": True, }):
+            {"quality": list_get(sys.argv, 8, "medium_quality"), "preview": True, }):
         try:
-            prod_y_min = float(sys.argv[5])
-            prod_y_max = float(sys.argv[6])
-            x_min = float(sys.argv[7])
-            x_max = float(sys.argv[8])
-            x_step = float(sys.argv[9])
+            x_min = float(sys.argv[5])
+            x_max = float(sys.argv[6])
+            x_step = float(sys.argv[7])
         except ValueError:
             print('The x-domain and y-range values must be numbers. Please consult the README.')
             return
 
-        scene = AnimatedConvolution(sys.argv[2], sys.argv[4], sys.argv[1], sys.argv[3], [x_min, x_max, x_step],
-                                    [prod_y_min, prod_y_max])
+        scene = AnimatedConvolution(sys.argv[2], sys.argv[4], sys.argv[1], sys.argv[3], [x_min, x_max, x_step])
         scene.render()
 
 
